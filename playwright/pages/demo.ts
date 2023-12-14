36cf245220d09782c169325f3972b78e5a346c4e
Ahmet Gökhan Kocaman <ggokhankocaman@gmail.com>
2023-12-14 10:58:26 +0300

    deneme2

--- playwright/pages/demo.ts
+++ playwright/pages/demo.ts
@@ -7,7 +7,7 @@ export class DemoPage {
 
     constructor(page: Page) {
         this.page = page
-        this.homePageLabelCimri = page.locator("a[aria-label='cimri anasayfa']")
+        this.homePageLabelCimri = page.locator('a[aria-label="cimri anasayfa"]')
         this.homePageLabelCimriMarkette = page.locator("a[aria-label='cimri markette']")
         this.homePageLabelCimriFinans = page.locator("div:has(>a[aria-label='cimri anasayfa']) > a:nth-of-type(3)")
         
