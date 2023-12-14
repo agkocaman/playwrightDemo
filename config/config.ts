2d1a375b602a465bcdccf88455a1410057d92f66
Ahmet Gökhan Kocaman <ggokhankocaman@gmail.com>
2023-12-14 15:42:55 +0300

    main category

--- config/config.ts
+++ config/config.ts
@@ -7,5 +7,9 @@ if (!env) {
     ...((env === '1') && {
         baseURL: 'https://www.cimri.com/'
       }),
+      ...((env === '2') && {
+        baseURL: 'https://pilot.cimri.com/'
+      }),
 
   }
+  export const mainCategories = ["elektronik","ev-yasam-ofis-kirtasiye","anne-bebek-oyuncak","saat-moda-taki","kitap-muzik-hobi","spor-outdoor","saglik-bakim-kozmetik","oto-bahce-yapi-market","pet"] 
--- playwright/tests/demo/demo.e2e.ts
+++ playwright/tests/demo/demo.e2e.ts
@@ -1,7 +1,7 @@
 
 import { expect } from "@playwright/test"
 import { DemoPage } from "../../pages/demo"
-import { configEnv } from "../../../config/config";
+import { configEnv, mainCategories } from "../../../config/config";
 
 
 let demoPage : DemoPage
@@ -15,8 +15,8 @@ export const DemoWeb = {
 
     E2E: {
         demoTest: {
-            notes: "demo",
-            tag: "",
+            notes: "homePageTitle",
+            tag: "Cimri anasayfa title kontrol",
             run: async ({ page }) => {
                 await page.goto(`${configEnv.baseURL}`, { timeout: 10000 })
                 await expect(page).toHaveTitle("Cimri - En son kaça olur?")
@@ -30,9 +30,23 @@ export const DemoWeb = {
                 await expect(demoPage.homePageLabelCimri).toHaveText("cimri")
                 await expect(demoPage.homePageLabelCimriMarkette).toHaveText("cimri markette")
                 await expect(demoPage.homePageLabelCimriFinans).toHaveText("cimri finans")
-            }
+                const response = await page.request.get('https://cimri.com');
+                await expect(response).toBeOK();
+          },
         },
+        mainCategoryStatus: {
+            notes: "Cimri ana kategori status kontrol",
+            tag: "",
+            only:true,
+            run: async ({ page }) => {
+                let i: number = 0;
+                do {
+                    const response = await page.request.get(`${configEnv.baseURL}`+`${mainCategories[i]}`);
+                    await expect(response).toBeOK()
+                    i++;
+                } while ( i < mainCategories.length)
+          }
+        }
     }
+
 }
-
-
